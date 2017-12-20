0757e00730e824209d7b86fe4622fd47584151cd
Samson <league@WTS19.attlocal.net>
2017-12-19 18:28:07 -0800

    soundtrack to life IP
    ;-;

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -12,14 +12,14 @@ public class SoundtrackToLife {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
 		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "", "Mood2", "Mood3" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
+System.out.println(userMood);
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
-
-		
+if (userMood==0) {
+	playVideo("http://www.youtube.com/watch?v=y-HKSdLms8Y");
+}				
 		// 4. Play different songs for other moods.
 
 /**
@@ -28,8 +28,6 @@ public class SoundtrackToLife {
 *	http://bit.ly/video-for-sad
 *	http://bit.ly/video-for-angry
 **/
-		
-		// If you are seeing ads at the beginning of your videos, install Adblock.
 
 	}
 
